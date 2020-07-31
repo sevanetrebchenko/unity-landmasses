810ec419042da3cae3ebfccaa12f8d78f89824a1
Seva Netrebchenko <seva.netrebchenko@gmail.com>
2020-07-30 18:40:04 -0700

    Added texture generator.

---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextureGenerator {
+    // Get the map texture based on a color array.
+    public static Texture2D TextureFromColorMap(int mapWidth, int mapHeight, Color[] textureColorMap) {
+        Texture2D mapTexture = new Texture2D(mapWidth, mapHeight);
+
+        // Prevent blurry pixels.
+        mapTexture.filterMode = FilterMode.Point;
+        mapTexture.wrapMode = TextureWrapMode.Clamp;
+
+        mapTexture.SetPixels(textureColorMap);
+        mapTexture.Apply();
+
+        return mapTexture;
+    }
+
+    // Get the map texture based on a height array.
+    public static Texture2D TextureFromHeightMap(float[,] heightMap) {
+        // Get map dimensions.
+        int mapWidth = heightMap.GetLength(0);
+        int mapHeight = heightMap.GetLength(1);
+
+        // It's faster to generate an array of pixel colors and set them all at once in the texture than one by one.
+        Color[] textureColorMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; ++y) {
+            for (int x = 0; x < mapWidth; ++x) {
+                int colorIndex = x + mapWidth * y;
+                textureColorMap[colorIndex] = Color.Lerp(Color.black, Color.white, heightMap[x, y]);
+            }
+        }
+
+        return TextureFromColorMap(mapWidth, mapHeight, textureColorMap);
+    }
+}
