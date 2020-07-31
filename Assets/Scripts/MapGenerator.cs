0d5afee51d9503757ab7971062db8f94dae10f3f
Seva Netrebchenko <seva.netrebchenko@gmail.com>
2020-07-30 18:06:40 -0700

    Added noise octaves and editor features to support editing in the game scene.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,16 +6,45 @@ public class MapGenerator : MonoBehaviour {
 
     public int mapWidth;
     public int mapHeight;
+
     public float noiseScale;
+
+    public int numNoiseOctaves;
+    [Range(0, 1)]
+    public float persistence;
+    public float lacunarity;
+    public Vector2 offset;
+
+    public int mapSeed;
 
     public bool autoUpdate;
 
     public void GenerateMap() {
         // Retrieve noise map.
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, mapSeed, noiseScale, numNoiseOctaves, persistence, lacunarity, offset);
 
         // Display noise map.
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    // Automatically called when variables are changed in the inspector.
+    // Used to verify (and clamp) data.
+    private void OnValidate() {
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+
+        if (numNoiseOctaves < 0) {
+            numNoiseOctaves = 0;
+        }
+
+        if (lacunarity < 1.0f) {
+            lacunarity = 1.0f;
+        }
     }
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -5,23 +5,66 @@ using UnityEngine;
 public static class Noise {
 
     // Returns a grid of values between 0 and 1
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float noiseScale) {
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int mapSeed, float noiseScale, int numNoiseOctaves, float persistence, float lacunarity, Vector2 manualOffset) {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random randomNumberGenerator = new System.Random(mapSeed);
+        Vector2[] octaveOffets = new Vector2[numNoiseOctaves];
+
+        for (int i = 0; i < numNoiseOctaves; ++i) {
+            float offsetX = randomNumberGenerator.Next(-100000, 100000) + manualOffset.x;
+            float offsetY = randomNumberGenerator.Next(-100000, 100000) + manualOffset.y;
+
+            octaveOffets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (noiseScale <= 0) {
             noiseScale = 0.0001f;
         }
 
+        float maximumNoiseHeight = float.MinValue;
+        float minimumNoiseHeight = float.MaxValue;
+
+        // Changing noise scale should zoom noise in and out from the center of the screen rather than top left.
+        float halfWidth = mapWidth / 2.0f;
+        float halfHeight = mapHeight / 2.0f;
+
         // Generate perlin noise values in the map.
         for (int y = 0; y < mapHeight; ++y) {
             for (int x = 0; x < mapWidth; ++x) {
-                float sampleX = x / noiseScale;
-                float sampleY = y / noiseScale;
 
-                // Perlin noise gets the same value each time if the arguments passed are integer values.
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                float amplitude = 1.0f;
+                float frequency = 1.0f;
+                float noiseHeight = 0.0f;
 
-                noiseMap[x, y] = perlinValue;
+                for (int i = 0; i < numNoiseOctaves; ++i) {
+                    float sampleX = (x - halfWidth) / noiseScale * frequency + octaveOffets[i].x;
+                    float sampleY = (y - halfHeight) / noiseScale * frequency + octaveOffets[i].y;
+
+                    // Perlin noise gets the same value each time if the arguments passed are integer values.
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1.0f; // Get in the range -1 to 1.
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistence; // Persistence should be between 0 and 1 - amplitude decreases with each octave.
+                    frequency *= lacunarity;  // Lacunarity should be greater than 1 - frequency increases with each octave.
+
+                    // Get the highest and lowest noise map values.
+                    if (noiseHeight > maximumNoiseHeight) {
+                        maximumNoiseHeight = noiseHeight;
+                    }
+                    else if (noiseHeight < minimumNoiseHeight) {
+                        minimumNoiseHeight = noiseHeight;
+                    }
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        // Normalize noise map values back to range [0, 1]
+        for (int y = 0; y < mapHeight; ++y) {
+            for (int x = 0; x < mapWidth; ++x) {
+                noiseMap[x, y] = Mathf.InverseLerp(minimumNoiseHeight, maximumNoiseHeight, noiseMap[x, y]);
             }
         }
 
